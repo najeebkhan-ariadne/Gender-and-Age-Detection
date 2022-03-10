a42166567f346ae46abc4244fb9620f87c0b802f
najeebkhan-ariadne <najeeb@ariadnemaps.com>
2022-03-10 09:55:49 -0500

    [feat] Person detector on a video sample

--- detect.py
+++ detect.py
@@ -5,6 +5,7 @@ import math
 import argparse
 import numpy as np
 import pyrealsense2 as rs
+from time import sleep
 
 def init_realsense():
     rs_pipeline = rs.pipeline()
@@ -32,26 +33,16 @@ def get_frame(rs_pipeline):
     img = get_image(color)
     return img
 
-def highlightFace(net, frame, conf_threshold=0.7):
-    frameOpencvDnn=frame.copy()
-    frameHeight=frameOpencvDnn.shape[0]
-    frameWidth=frameOpencvDnn.shape[1]
-    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+def highlightPerson(hog, frame):
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8))
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
 
-    net.setInput(blob)
-    detections=net.forward()
-    faceBoxes=[]
-    for i in range(detections.shape[2]):
-        confidence=detections[0,0,i,2]
-        if confidence>conf_threshold:
-            x1=int(detections[0,0,i,3]*frameWidth)
-            y1=int(detections[0,0,i,4]*frameHeight)
-            x2=int(detections[0,0,i,5]*frameWidth)
-            y2=int(detections[0,0,i,6]*frameHeight)
-            faceBoxes.append([x1,y1,x2,y2])
-            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
-    return frameOpencvDnn,faceBoxes
-
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
+    
+    return frame, boxes
 
 parser=argparse.ArgumentParser()
 parser.add_argument('--image')
@@ -69,30 +60,48 @@ MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList=['Male','Female']
 
-faceNet=cv2.dnn.readNet(faceModel,faceProto)
+# faceNet=cv2.dnn.readNet(faceModel,faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 # video=cv2.VideoCapture(args.image if args.image else 0)
 padding=20
 
-rs_pipeline = init_realsense()
+# rs_pipeline = init_realsense()
 
-while cv2.waitKey(1) < 0 :
+cap = cv2.VideoCapture('stock-footage.webm')
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+out = cv2.VideoWriter('result/output.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
+count = 0
+# while cv2.waitKey(1) < 0 :
+while cap.isOpened():
     # hasFrame,frame=video.read()
-    img = get_frame(rs_pipeline)
-    
-    if img is None:
+    # img = get_frame(rs_pipeline)
+    ret, frame = cap.read()
+
+    if not ret:
+        cap.release()
+        out.release()
+        break
+
+    if frame is None:
         cv2.waitKey()
         break
     
-    resultImg, faceBoxes = highlightFace(faceNet,img)
+    resultImg, personBoxes = highlightPerson(hog, frame)
     
-    if not faceBoxes:
+    if personBoxes is None:
         cv2.imshow("Detecting age and gender", resultImg)
         print("No face detected")
 
-    for faceBox in faceBoxes:
+    for faceBox in personBoxes:
         face=frame[max(0,faceBox[1]-padding):
                    min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
                    :min(faceBox[2]+padding, frame.shape[1]-1)]
@@ -110,3 +119,6 @@ while cv2.waitKey(1) < 0 :
 
         cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
         cv2.imshow("Detecting age and gender", resultImg)
+        cv2.imwrite(f"/home/ikea_ottawa_1/Gender-and-Age-Detection/result/test_{count}.jpg", resultImg)
+        out.write(resultImg)
+        count += 1
